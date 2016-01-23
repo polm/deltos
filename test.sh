4bfeac894bd97bde2e6a95115f8226029cea309f
Paul O'Leary McCann <polm@dampfkraft.com>
2016-01-23 18:11:13 +0900

    Add basic test script

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Run some simple tests
+set -e
+set -u
+set -o pipefail
+
+cd "$(dirname "$0")"
+export DELTOS_HOME=./dtest
+
+deltos="./bin/deltos"
+
+$deltos init
+$deltos tsv > /dev/null
+$deltos json > /dev/null
+./bin/deltos-cache
+
+# to test creation, make a file...
+newtest=$($deltos new Testy)
+# add a random identifier
+testwords=$(echo -n "$RANDOM")
+echo "$testwords" >> $newtest
+# build the site
+$deltos build-site
+# and make sure it shows up in the html output
+grep "$testwords" $DELTOS_HOME/private/by-id/$(basename $newtest).html
+
+# TODO
+# - maybe figure how to test interactive parts?
+# - check RSS
+# - check exclusion / published tag use
